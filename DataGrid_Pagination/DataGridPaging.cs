7ae61142b8c5b33bbf1687e10abb2429d6785823
Isaac Be <esaaco@gmail.com>
2019-11-01 22:01:56 +0200

    Add files via upload

---
+++ DataGrid_Pagination/DataGridPaging.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public class SuperGrid : DataGridView
+{
+    public int PageSize
+    {
+        get
+        {
+            return _pageSize;
+        }
+        set
+        {
+            _pageSize = value;
+        }
+    }
+    public int _pageSize = 10;
+    BindingSource bs;//= new BindingSource();
+    BindingList<DataTable> tables;// = new BindingList<DataTable>();
+    public void SetPagedDataSource(DataTable dataTable, BindingNavigator bnav)
+    {
+        if (dataTable ==null || bnav ==null)
+        {
+            return;
+        }
+
+        DataTable dt = null;
+        bs = new BindingSource();
+        tables = new BindingList<DataTable>();
+        int counter = 1;
+       
+        foreach (DataRow dr in dataTable.Rows)
+        {
+            
+                if (counter == 1)
+                {
+                    dt = dataTable.Clone();
+                    tables.Add(dt);
+                     
+                }
+
+                dt.Rows.Add(dr.ItemArray);
+                if (PageSize < ++counter)
+                {
+                    counter = 1;
+                }
+            
+           
+
+
+            }
+        bnav.BindingSource = bs;
+        bs.DataSource = tables;
+        bs.PositionChanged += bs_PositionChanged;
+        bs_PositionChanged(bs, EventArgs.Empty);
+    }
+    void bs_PositionChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            this.DataSource = tables[bs.Position];
+        }catch(Exception ex) {
+
+            MessageBox.Show(ex.Message);
+        }
+    }
+}
---
+++ DataGrid_Pagination/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGrid_Pagination
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Col1");
+            dt.Columns.Add("Col2");
+            dt.Columns.Add("Col3");
+            dt.Columns.Add("Col4");
+            dt.Columns.Add("Col5");
+            dt.Columns.Add("Col6");
+            dt.Columns.Add("Col7");
+            string[] Lines = File.ReadAllLines("ApplicationDocs.txt");
+            foreach (string line in Lines )
+            {
+                string[] split = line.Split('\t');
+                dt.Rows.Add(
+                    split[0].ToString(),
+                     split[1].ToString(),
+                      split[2].ToString(),
+                       split[3].ToString(),
+                        split[4].ToString(),
+                         split[5].ToString(),
+                          split[6].ToString()
+                    );
+            }
+
+            superGrid1 .SetPagedDataSource(dt, bindingNavigator1);
+            // set paging size 
+            superGrid1 .PageSize = 100;
+        }
+    }
+}
